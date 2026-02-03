579f597c14b30527ceda4dfa6141deefbdf533db
Mike Hardy <github@mikehardy.net>
2026-02-03 08:01:43 -0500

    feat: adopt google-java-format 1.34.0 w/instance main method support

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function spawnGoogleJavaFormat(args, done, stdio) {
   // Add the library in, with java 16 compat
   args = [
     "-jar",
-    `${LOCATION}/lib/google-java-format-1.32.0-all-deps.jar`,
+    `${LOCATION}/lib/google-java-format-1.34.0-all-deps.jar`,
   ].concat(args);
 
   // extract glob, if present
